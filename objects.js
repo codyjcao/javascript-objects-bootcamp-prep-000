692affeb0802dcc028cbe1f74726d49e370b4d96
cjc7@williams.edu Cao <cjc7@williams.edu>
2018-01-27 20:39:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,5 @@ var meals = {};
 var meals = new Object();
 var meals = { breakfast: "oatmeal"}
 var meals = new Object({breakfast: 'oatmeal'})
+
+var meals = { breakfast: 'eggs', breakfast: 'bacon'}
